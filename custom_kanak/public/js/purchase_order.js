e46ccefbbed2cc91dd8301670af3d0e70852c48b
cod-odoo <cod@kanakinfosystems.com>
2024-02-07 10:28:56 +0530

    [FIX]Fixed rate field edit issue.

--- custom_kanak/public/js/purchase_order.js
+++ custom_kanak/public/js/purchase_order.js
@@ -1,19 +1,25 @@
 frappe.ui.form.on('Purchase Order Item', {
     rate(frm) {
-        frappe.call({
-            method: "validate_rate_with_range",
-            doc: frm.doc,
-            args:{
-                'items':frm.doc.items
-            },
-            callback: (r) => {
-                if (r._server_messages){
-                    frm.fields_dict.items.grid.toggle_enable('rate', 1); 
-                }
-                else{
-                    frm.fields_dict.items.grid.toggle_enable('rate', 0);
-                }
-            },
+        $(frm.wrapper).on("grid-row-render", function(e, grid_row) {
+            if(grid_row.grid.df.fieldname=="items"){
+                frappe.call({
+                    method: "validate_rate_with_range",
+                    doc: frm.doc,
+                    args:{
+                        'line':grid_row.doc
+                    },
+                    callback: (r) => {
+                        if (r._server_messages){
+                            grid_row.toggle_editable("rate", true);
+                            grid_row.toggle_enable("rate", true);
+                        }
+                        else{
+                            grid_row.toggle_editable("rate", false);
+                            grid_row.toggle_enable("rate", false);
+                        }
+                    },
+                });
+            }
         });
     }
 })
