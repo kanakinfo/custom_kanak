cdb6eb2e89e3661be3776289bd6ac7cdfabf0850
Kanak Infosystems LLP <info@kanakinfosystems.com>
2024-02-13 18:24:28 +0530

    Update purchase_order.js

--- custom_kanak/public/js/purchase_order.js
+++ custom_kanak/public/js/purchase_order.js
@@ -73,63 +73,63 @@ frappe.show_alert = frappe.toast = function (message, seconds = 7, actions = {})
 
     return div;
 };
-var super_setup_queries = erpnext.buying.BuyingController.prototype.setup_queries;
+// var super_setup_queries = erpnext.buying.BuyingController.prototype.setup_queries;
 
-// Override the setup_queries method
-erpnext.buying.BuyingController.prototype.setup_queries = function() {
-    // Call the original method with the same arguments as passed to this method
-    super_setup_queries.apply(this, arguments);
+// // Override the setup_queries method
+// erpnext.buying.BuyingController.prototype.setup_queries = function() {
+//     // Call the original method with the same arguments as passed to this method
+//     super_setup_queries.apply(this, arguments);
 
-    this.frm.set_query("item_code", "items", function() {
-            if (me.frm.doc.is_subcontracted) {
-                var filters = {'supplier': me.frm.doc.supplier};
-                if (me.frm.doc.is_old_subcontracting_flow) {
-                    filters["is_sub_contracted_item"] = 1;
-                }
-                else {
-                    filters["is_stock_item"] = 0;
-                }
+//     this.frm.set_query("item_code", "items", function() {
+//             if (me.frm.doc.is_subcontracted) {
+//                 var filters = {'supplier': me.frm.doc.supplier};
+//                 if (me.frm.doc.is_old_subcontracting_flow) {
+//                     filters["is_sub_contracted_item"] = 1;
+//                 }
+//                 else {
+//                     filters["is_stock_item"] = 0;
+//                 }
 
-                return{
-                    query: "erpnext.controllers.queries.item_query",
-                    filters: filters
-                }
-            }
-            else {
-                return{
-                    query: "erpnext.controllers.queries.item_query",
-                    filters: { 'supplier': me.frm.doc.supplier, 'is_purchase_item': 1, 'has_variants': 0, 'item_name': ['not like', '%-ROUT']}
-                }
-            }
-    });
-};
-frappe.ui.form.on('Purchase Order Item', {
-    rate(frm) {
-        $(frm.wrapper).on("grid-row-render", function(e, grid_row) {
-            if (grid_row.grid.df.fieldname == "items") {
-                frappe.call({
-                    method: "validate_rate_with_range",
-                    doc: frm.doc,
-                    args: {
-                        'line': grid_row.doc
-                    },
-                    callback: (r) => {
+//                 return{
+//                     query: "erpnext.controllers.queries.item_query",
+//                     filters: filters
+//                 }
+//             }
+//             else {
+//                 return{
+//                     query: "erpnext.controllers.queries.item_query",
+//                     filters: { 'supplier': me.frm.doc.supplier, 'is_purchase_item': 1, 'has_variants': 0, 'item_name': ['not like', '%-ROUT']}
+//                 }
+//             }
+//     });
+// };
+// frappe.ui.form.on('Purchase Order Item', {
+//     rate(frm) {
+//         $(frm.wrapper).on("grid-row-render", function(e, grid_row) {
+//             if (grid_row.grid.df.fieldname == "items") {
+//                 frappe.call({
+//                     method: "validate_rate_with_range",
+//                     doc: frm.doc,
+//                     args: {
+//                         'line': grid_row.doc
+//                     },
+//                     callback: (r) => {
                        
-                        if(r._server_messages){
-                            $(".rederrorcustomcl").remove();
-                            $(".orangeerrorcustom_cl").remove();
+//                         if(r._server_messages){
+//                             $(".rederrorcustomcl").remove();
+//                             $(".orangeerrorcustom_cl").remove();
                             
-                            grid_row.toggle_editable("rate", true);
-                            grid_row.toggle_enable("rate", true);
-                        }else {
-                             $(".rederrorcustomcl").remove();
-                             $(".orangeerrorcustom_cl").remove();
-                            grid_row.toggle_editable("rate", false);
-                            grid_row.toggle_enable("rate", false);
-                        }
-                    },
-                });
-            }
-        });
-    }
-});
+//                             grid_row.toggle_editable("rate", true);
+//                             grid_row.toggle_enable("rate", true);
+//                         }else {
+//                              $(".rederrorcustomcl").remove();
+//                              $(".orangeerrorcustom_cl").remove();
+//                             grid_row.toggle_editable("rate", false);
+//                             grid_row.toggle_enable("rate", false);
+//                         }
+//                     },
+//                 });
+//             }
+//         });
+//     }
+// });
